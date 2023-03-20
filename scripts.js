3844d5cc1ad666cd2d2f80c3d700cbbf87324a9e
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2023-03-20 00:18:59 -0300

    Update-responsiveness

--- scripts.js
+++ scripts.js
@@ -8,3 +8,16 @@ function trocaImagem(endereco) {
 function trocaCor(cor) {
     circulo.style.background = cor
 }
+
+var teste = 0;
+setInterval(piscar, 800);
+function piscar() {
+    if (teste < 1) {
+        teste++;
+        document.getElementById('piscando').style.opacity = '1';
+    } else {
+        teste = 0;
+        document.getElementById('piscando').style.opacity = '0';
+    }
+};
+
